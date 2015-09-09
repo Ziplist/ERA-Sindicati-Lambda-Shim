a3e9246001af7fcd632304b62e2b14367569311a
Cary FitzHugh <cary.fitzhugh@gmail.com>
2015-09-09 12:05:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,37 @@
-﻿// Load up all dependencies
-var AWS = require('aws-sdk');
+console.log('Loading function');
 
-console.log("Version 0.1.0");
+var aws = require('aws-sdk');
+var s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-// get reference to S3 client 
-var s3 = new AWS.S3();
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
 
-// This is the entry-point to the Lambda function.
-exports.handler = function (event, context) {
-        
-    // Process all records in the event.
-    for (var i = 0; i < event.Records.length; ++i) {
-        
-        // The source bucket and source key are part of the event data
-        var srcBucket = event.Records[i].s3.bucket.name;
-        var srcKey = unescape(event.Records[i].s3.object.key);
-        
-        // Get the target bucket based on the source bucket.
-        // Once we have that, perform the copy.
-        getTargetBucket(srcBucket, function (targetBucket) {
-            console.log("Copying '" + srcKey + "' from '" + srcBucket + "' to '" + targetBucket + "'");
+    // Get the object from the event and show its content type
+    var bucket = event.Records[0].s3.bucket.name;
+    var key = event.Records[0].s3.object.key;
+    var destination_bucket = "icerss.condenast.com";
+    var destination_path   = "/test/" + key.split("/").reverse()[0];
+    
+    var params = {
+        Bucket: destination_bucket,
+        Key: destination_path,
+        CopySource: bucket + "/" + key,
+        MetadataDirective: "COPY",
+        ACL: "public-read"
+    };
+    console.log('Copying file:', JSON.stringify(params, null, 2));
+ 
+    s3.copyObject(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+       console.log("Copied the file: ", data);           // successful response
+          
+       // s3.getObject({Bucket: destination_bucket, Key: destination_path}, function(err, data) {
+       //   if (err) console.log(err, err.stack); // an error occurred
+       //   else     console.log("Copied: " + JSON.stringify(data));           // successful response
+       // });
+       context.succeed("Copied the file!");     
+      }
+   });
 
-            var dstBucket = targetBucket;
-            var dstKey = srcKey;
-            
-            // Copy the object from the source bucket
-            s3.copyObject({
-                Bucket: dstBucket,
-                Key: dstKey,
-                
-                CopySource: escape(srcBucket + '/' + srcKey),
-                MetadataDirective: 'COPY'
-            }, function (err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else console.log(data);           // successful response
-            });
-        });
-    }
 };
-
-// getTargetBucket
-//
-// Gets the tags for the named bucket, and
-// from those tags, finds the "TargetBucket" tag.
-// Once found, it calls the callback function passing
-// the tag value as the single parameter.
-function getTargetBucket(bucketName, callback) {
-    console.log("Getting tags for bucket '" + bucketName + "'");
-    s3.getBucketTagging({
-        Bucket: bucketName
-    }, function (err, data) {
-        if (err) {
-            console.log(err, err.stack); // an error occurred
-            return;
-        }
-        
-        console.log(data);
-        var tags = data.TagSet;
-        
-        console.log("Looking for 'TargetBucket' tag");
-        for (var i = 0; i < tags.length; ++i) {
-            var tag = tags[i];
-            if (tag.Key == 'TargetBucket') {
-                console.log("Tag 'TargetBucket' found with value '" + tag.Value + "'");
-                callback(tag.Value);
-                return;
-            }
-        }
-        
-        console.log("Tag 'TargetBucket' not found");
-    });
-}
