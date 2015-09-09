9d3c5d775bd180b78812a8667e0e0e9f01d9cafd
Cary FitzHugh <cary.fitzhugh@gmail.com>
2015-09-09 12:25:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,23 +9,26 @@ exports.handler = function(event, context) {
     // Get the object from the event and show its content type
     var source_bucket = event.Records[0].s3.bucket.name;
     var source_key = event.Records[0].s3.object.key;
-    var destination_bucket = "icerss.condenast.com";
-    var destination_path   = "test/" + source_key.split("/").reverse()[0];
-    
-    var params = {
-        Bucket: destination_bucket,
-        Key: destination_path,
-        CopySource: source_bucket + "/" + source_key,
-        MetadataDirective: "COPY",
-        ACL: "public-read"
-    };
-    console.log('Copying file:', JSON.stringify(params, null, 2));
- 
-    s3.copyObject(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else {
-       context.succeed("Copied the file from " + source_bucket + "/" + source_key + " => " + destination_bucket + "/" + destination_path );     
-      }
-   });
-
+    if (source_key.match(/^msn-publications\//)) {
+        var destination_bucket = "icerss.condenast.com";
+        var destination_path   = "test/" + source_key.split("/").reverse()[0];
+        
+        var params = {
+            Bucket: destination_bucket,
+            Key: destination_path,
+            CopySource: source_bucket + "/" + source_key,
+            MetadataDirective: "COPY",
+            ACL: "public-read"
+        };
+        console.log('Copying file:', JSON.stringify(params, null, 2));
+     
+        s3.copyObject(params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else {
+           context.succeed("Copied the file from " + source_bucket + "/" + source_key + " => " + destination_bucket + "/" + destination_path );     
+          }
+       });
+    } else {
+        context.succeed("Skipped the file");
+    }
 };
